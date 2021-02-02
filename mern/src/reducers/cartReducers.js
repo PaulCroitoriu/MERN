6ce0e191769db39568547d5dadc7d27f66c35240
PaulCroitoriu <croitoriu.pavel@gmail.com>
2021-02-02 18:02:23 +0000

    cart screen

--- mern/src/reducers/cartReducers.js
+++ mern/src/reducers/cartReducers.js
@@ -9,15 +9,15 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
 
       if (existItem) {
         return {
-          ...state.cartItems,
-          item,
-        }
-      } else {
-        return {
           ...state,
           cartItems: state.cartItems.map(x =>
             x.product === existItem.product ? item : x
           ),
+        }
+      } else {
+        return {
+          ...state,
+          cartItems: [...state.cartItems, item],
         }
       }
     default:
